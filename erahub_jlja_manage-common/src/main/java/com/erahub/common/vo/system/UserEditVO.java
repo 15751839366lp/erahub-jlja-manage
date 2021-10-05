4c2803d2018fdf52bd3e7a185732d44594938805
李彭 <15751839366@163.com>
2021-10-05 15:43:29 +0800

    权限管理系统完善

--- erahub_jlja_manage-common/src/main/java/com/erahub/common/vo/system/UserEditVO.java
+++ erahub_jlja_manage-common/src/main/java/com/erahub/common/vo/system/UserEditVO.java
@@ -20,20 +20,20 @@ public class UserEditVO {
     @NotBlank(message = "用户名不能为空")
     private String username;
 
-    @NotBlank(message = "昵称不能为空")
+//    @NotBlank(message = "昵称不能为空")
     private String nickname;
 
-    @Email(message = "请输入正确的邮箱格式")
+//    @Email(message = "请输入正确的邮箱格式")
     private String email;
 
-    @NotBlank(message = "电话号码不能为空")
+//    @NotBlank(message = "电话号码不能为空")
     private String phoneNumber;
 
     @NotNull(message = "性别不能为空")
     private Integer sex;
 
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy年MM月dd日")
-    @NotNull(message = "生日不能为空")
+//    @NotNull(message = "生日不能为空")
     private Date birth;
 
     @NotNull(message = "部门不能为空")
