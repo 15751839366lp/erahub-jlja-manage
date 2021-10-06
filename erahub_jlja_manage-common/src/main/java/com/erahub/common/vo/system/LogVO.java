218698ad0cda99b69cec379d079c7f8287f93fa4
李彭 <15751839366@163.com>
2021-10-06 12:07:56 +0800

    权限管理系统完善

--- erahub_jlja_manage-common/src/main/java/com/erahub/common/vo/system/LogVO.java
+++ erahub_jlja_manage-common/src/main/java/com/erahub/common/vo/system/LogVO.java
@@ -22,7 +22,7 @@ public class LogVO {
 
     private String ip;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
     private String location;
