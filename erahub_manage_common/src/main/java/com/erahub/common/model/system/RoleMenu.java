bebe9b4f9d179b4df26a94ee33ed3c31042e9dbf
15751839366lp <15751839366@163.com>
2022-04-20 14:34:38 +0800

    增加id

--- erahub_manage_common/src/main/java/com/erahub/common/model/system/RoleMenu.java
+++ erahub_manage_common/src/main/java/com/erahub/common/model/system/RoleMenu.java
@@ -1,11 +1,16 @@
 package com.erahub.common.model.system;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
 @TableName("tb_role_menu")
 public class RoleMenu {
+
+    @TableId
+    private Long id;
+
     private Long roleId;
 
     private Long menuId;
--- erahub_manage_common/src/main/java/com/erahub/common/model/system/UserRole.java
+++ erahub_manage_common/src/main/java/com/erahub/common/model/system/UserRole.java
@@ -1,11 +1,15 @@
 package com.erahub.common.model.system;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
 @TableName("tb_user_role")
 public class UserRole {
+
+    @TableId
+    private Long id;
 
     private Long userId;
 
